30bee0a6703bb319211fd8795394db63f5ae2f9c
DenielK <denielkruusman@gmail.com>
2025-04-09 16:52:54 +0300

    Add files via upload

---
+++ FunktsioonideClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TARgv24
+{
+    class FunktsioonideClass
+    {
+        //Console.WriteLine("Arvuda {0} ja {1} korrutis võrdub{2}", arv1, arv2, arv1 * arv2);
+        public static float Kalkulaator(int arv1, int arv2)
+        {
+            float kalkulaator = 0;
+            kalkulaator = arv1 * arv2;
+            return kalkulaator;
+        }
+
+        public static string Random()
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(1, 8); // Генерация случайного числа от 1 до 7
+            string tekst = "";
+
+            switch (a)
+            {
+                case 1: tekst = "E"; break;
+                case 2: tekst = "T"; break;
+                case 3: tekst = "K"; break;
+                case 4: tekst = "N"; break;
+                case 5: tekst = "R"; break;
+                case 6: tekst = "L"; break;
+                case 7: tekst = "P"; break;
+                default: tekst = "Tundmatu"; break;
+            }
+
+            return tekst;
+        }
+
+        public static void Juku(string tekst)
+        {
+            if (tekst.ToLower() == "juku")
+            {
+                try
+                {
+                    Console.WriteLine("{0}\nKui vana sa oled?", tekst);
+                    int vanus = int.Parse(Console.ReadLine());
+                    if (vanus <= 0 || vanus > 100) // && и, || или
+                    {
+                        Console.WriteLine("Viga!");
+                    }
+                    else if (vanus <= 6)
+                    {
+                        Console.WriteLine("Tasuta!");
+                    }
+                    else if (vanus < 15)
+                    {
+                        Console.WriteLine("Lastepilet!");
+                    }
+                    else if (vanus < 65)
+                    {
+                        Console.WriteLine("Täispilet!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sooduspilet!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Olen hõivatud!");
+            }
+        }
+    }
+}
---
+++ MainClas.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24
+{
+    class MainClas
+    {
+        public static void Main(string[] args)
+        {
+            /*Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;*/
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World! Hea Päev!"); //"" строка  '' символ
+            // I.osa Andmetüübid, If, Case, Random, Alamfunktsioonid
+            /*
+             * 
+             * если надо заблокировать целые абзацы
+             * 
+             * 
+             */
+            int a = 0;
+            string tekst = "Python";
+            char taht = 'A';
+            double arv = 5.32423234;
+            float arv1 = 2;
+
+
+            Console.Write("Mis on sinu nimi? ");
+            tekst = Console.ReadLine();
+            Console.WriteLine("Tere!\n" + tekst);
+            Console.WriteLine("{1}Tere!\n {0}", tekst, arv);
+
+            // Вызов функции для обработки Juku
+            FunktsioonideClass.Juku(tekst);
+
+            Console.Write("Arv 2 :");
+            int arv2 = int.Parse(Console.ReadLine());
+            arv = FunktsioonideClass.Kalkulaator(a, arv2);
+            Console.WriteLine(arv1);
+
+            // Вызов функции для рандома
+            Console.WriteLine("Switch'i kasutamine");
+            string randomResult = FunktsioonideClass.Random();
+            Console.WriteLine(randomResult);
+
+            Console.ReadKey();
+        }
+    }
+}

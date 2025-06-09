ee6ca04daaa9ba2d9121304c20a2f066594d6c8e
DenielK <denielkruusman@gmail.com>
2025-06-09 18:04:15 +0300

    Add files via upload

---
+++ 4. osa/MainClass_4osa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOsad._4._osa
+{
+    internal class MainClass_4osa
+    {
+    }
+}
---
+++ 4. osa/Opilane.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOsad._4._osa
+{
+    internal class Opilane
+    {
+        public string Nimi { get; set; }
+        public List<int> Hinded { get; set; }
+
+        public Opilane(string nimi, List<int> hinded)
+        {
+        Nimi = nimi;
+        Hinded = hinded;
+        }
+
+        public double KeskmineHinne()
+        {
+            return Math.Round(Hinded.Average(), 2);
+        }
+    }
+}
